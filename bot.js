23f3deb1a46f9e7379d5862a70ed082eabbdfffb
programgames <julgamer1@gmail.com>
2020-03-29 13:49:19 +0200

    change music dir

--- bot.js
+++ bot.js
@@ -49,7 +49,7 @@ client.on('message', message => {
             return message.channel.send(`Aucun meme spécifié, ${message.author}!`);
         }
         voiceChannel.join().then(connection => {
-            var filePath = './memes/' + args[0] + '.mp3';
+            var filePath = '../discordmemesSf/public/upload/mp3' + args[0] + '.mp3';
             if (!fs.existsSync(filePath)) {
                 return message.channel.send(`Le fichier, ${filePath} n'existe pas`);
             }
